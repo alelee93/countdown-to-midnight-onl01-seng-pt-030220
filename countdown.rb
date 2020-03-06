4fe754fd06b8973c539add6e061fada165fe534a
Alejandra I. Lee Vargas <leevargasale@gmail.com>
2020-03-06 02:41:54 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(number)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(num)
+  while num > 0
+   puts "#{number} SECOND(S)!"
+   number -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+  
